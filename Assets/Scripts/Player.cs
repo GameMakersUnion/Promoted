59f493e3bf4dda7d63fe0630fbbfcb9371e9bdd6
Victor <victor.nguyen@hotmail.com>
2015-05-02 14:22:58 -0400

    isControllable = true/false, to disable player controlls

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,6 +7,7 @@ public class Player : MonoBehaviour {
 	public float jumpPower = 10;
 	Rigidbody2D body;
 	int layerMask = 1 << 2;
+    public bool isControllable; //I know public access is bad, =P - Victor
 
     private enum Action {  Left, Right, Jump, Elevate, Action1, Hold };
 
@@ -26,7 +27,8 @@ public class Player : MonoBehaviour {
     void Start () {
 		body = gameObject.GetComponent<Rigidbody2D> ();
 		layerMask = ~layerMask;
-	}
+        isControllable = true;
+    }
 	
 	// Update is called once per frame
 	void Update ()
@@ -36,41 +38,43 @@ public class Player : MonoBehaviour {
 
     void FixedUpdate()
     {
-        // Horizontal movement
-        if (Input.GetKey(Do[Action.Left]))
-        {
-            transform.localScale = new Vector3(1f, 1f, 1f);
-            body.velocity = new Vector2(-speed, body.velocity.y);
-        }
-        else if (Input.GetKey(Do[Action.Right]))
-        {
-            transform.localScale = new Vector3(-1f, 1f, 1f);
-            body.velocity = new Vector2(speed, body.velocity.y);
-        }
-        // Vertical movement
-        if (Input.GetKeyDown(Do[Action.Jump]))
-        {
-            if (Physics2D.Raycast(transform.position, Vector3.down, legs*1.1f, layerMask))
+        if (isControllable) { 
+            // Horizontal movement
+            if (Input.GetKey(Do[Action.Left]))
             {
-                body.AddForce(new Vector2(0, jumpPower), ForceMode2D.Impulse);
+                transform.localScale = new Vector3(1f, 1f, 1f);
+                body.velocity = new Vector2(-speed, body.velocity.y);
             }
-        }
-        // Elevate 
-        if (Input.GetKey(Do[Action.Elevate]))
-        {
-            //liam does this
-        }
+            else if (Input.GetKey(Do[Action.Right]))
+            {
+                transform.localScale = new Vector3(-1f, 1f, 1f);
+                body.velocity = new Vector2(speed, body.velocity.y);
+            }
+            // Vertical movement
+            if (Input.GetKeyDown(Do[Action.Jump]))
+            {
+                if (Physics2D.Raycast(transform.position, Vector3.down, legs*1.1f, layerMask))
+                {
+                    body.AddForce(new Vector2(0, jumpPower), ForceMode2D.Impulse);
+                }
+            }
+            // Elevate 
+            if (Input.GetKey(Do[Action.Elevate]))
+            {
+                //liam does this
+            }
 
-        // Action1 (launch mail)
-        if (Input.GetKeyDown(Do[Action.Action1]))
-        {
-            //get from script attached to level Action Map
-        }
+            // Action1 (launch mail)
+            if (Input.GetKeyDown(Do[Action.Action1]))
+            {
+                //get from script attached to level Action Map
+            }
 
-        // Hold 
-        if (Input.GetKey(Do[Action.Hold]))
-        {
+            // Hold 
+            if (Input.GetKey(Do[Action.Hold]))
+            {
             
+            }
         }
     }
 }
