2e7a273f7fe231348785cf3358218aef91604e13
Victor <victor.nguyen@hotmail.com>
2015-05-02 16:20:13 -0400

    Starting Marketing Stage

---
+++ Assets/Scripts/Marketing/MarketingCanvas.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MarketingCanvas : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if(other.tag == "MarketingPaint")
+        {
+
+        }
+    }
+}
