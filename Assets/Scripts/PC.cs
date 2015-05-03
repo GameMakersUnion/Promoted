11cdb1c79ddee2f901a287e5f1aead19cb71d8df
Ian Nastajus <ian.nastajus@gmail.com>
2015-05-02 21:33:41 -0400

    PC fire stuff working!
    !@#$ING FUCK WASTED 3 HOURS OF MY LIFE FIXING WHAT WORKED EARLIER.

--- Assets/Scripts/PC.cs
+++ Assets/Scripts/PC.cs
@@ -1,17 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PC : MonoBehaviour
+public class PC : PCManager
 {
 
     private BoxCollider2D box;
     private SpriteRenderer renderer;
+    private Animator animator;
     private bool nearby = false;
     private Player player;
 
-    private const float timeToBoot = 3f;
-    private float timeToCrash;
-    private float timeRunning = 0;
+    private const float TIME_TO_BOOT = 3f;
+    private const float TIME_TO_FLAMES = 3f;
+    private float timeToCrashStart; 
+    private float timeToCrash; //assigned on start
+    private float health = 5f;  
+
+
+    private float timeCrashed = 0;
+    private float timeBooting = 0;
+
+    private enum State { Booting, Stable, Crashed, Flaming, Burnt }
+
+    private State state = State.Stable;
     
 
     // Use this for initialization
@@ -19,9 +30,88 @@ public class PC : MonoBehaviour
         box = gameObject.AddComponent<BoxCollider2D>();
         box.isTrigger = true;
         renderer = gameObject.GetComponent<SpriteRenderer>();
+        animator = gameObject.GetComponent<Animator>();
+        animator.SetBool("stable", true);
         player =  GameObject.FindWithTag("Player").GetComponent<Player>();
-        Random.seed = 42;
-        timeToCrash = Random.Range(timeToBoot + 1f, timeToBoot + 10f);
+        timeToCrashStart = Random.Range(TIME_TO_BOOT + 1f, TIME_TO_BOOT + 1f);
+        timeToCrash = timeToCrashStart;
+        PCs.Add(this);
+    }
+
+    private void Update()
+    {
+
+        Debug.Log(this.name + ": " + state + ", " + timeToCrash +","+ timeBooting + ", " + timeCrashed + ", " + health);
+        //exit early
+        if (state == State.Burnt)
+        {
+            return;
+        }
+
+        //rebooting
+        if (state == State.Booting && timeBooting < TIME_TO_BOOT)
+        {
+            timeBooting += Time.deltaTime;
+        }
+
+        //go stable
+        else if (state == State.Booting && timeBooting >= TIME_TO_BOOT)
+        {
+            state = State.Stable;
+            animator.SetBool("booting", false);
+            animator.SetBool("stable", true);
+            timeBooting = 0;
+            timeToCrash = timeToCrashStart;
+        }
+
+
+        //unstable 'puter
+        if (state == State.Stable && timeToCrash > 0)
+        {
+            timeToCrash -= Time.deltaTime;
+        }
+
+        //go blue screen
+        if (state == State.Stable && timeToCrash <= 0)
+        {
+            state = State.Crashed;
+            animator.SetBool("stable", false);
+            animator.SetBool("crashed", true);
+            Debug.Log("CRASHED");
+        }
+
+        //VERY unstable 'puter
+        if (state == State.Crashed)
+        {
+            timeCrashed += Time.deltaTime;
+        }
+
+        //go on fire
+        if (state == State.Crashed && timeCrashed > TIME_TO_FLAMES)
+        {
+            state = State.Flaming;
+            animator.SetBool("crashed", false);
+            animator.SetBool("flaming", true);
+        }
+
+        //hurt 'puter
+        if (state == State.Flaming)
+        {
+            health -= Time.deltaTime;
+
+        }
+
+        //go burnt
+        if (state == State.Flaming && health <= 0)
+        {
+            state = State.Burnt;
+//            animator.StopPlayback();
+            animator.SetBool("flaming", false);
+            animator.SetBool("crashed", true); //same thing
+//            animator.StartPlayback();
+            renderer.color = new Color(.2f, .2f, .2f);
+        }
+
     }
 
     private void FixedUpdate()
@@ -32,10 +122,17 @@ public class PC : MonoBehaviour
         }
 
         //do main logic here
-        if (nearby && player.activating)
+        if (nearby && player.activating && (state == State.Crashed || state == State.Flaming))
         {
-             this.renderer.color = Color.blue;
-            
+            state = State.Booting;
+            timeBooting = 0;
+            timeCrashed = 0;
+            timeToCrash = timeToCrashStart;
+
+            animator.SetBool("crashed", false);
+            animator.SetBool("flaming", false);
+            animator.SetBool("booting", true);
+
         }
 
 
--- Assets/Scripts/PCManager.cs
+++ Assets/Scripts/PCManager.cs
@@ -4,52 +4,21 @@ using System.Collections.Generic;
 
 public class PCManager : MonoBehaviour
 {
-
-    private Bounds room;
-    private Bounds pc;
-    private GameObject go;
-    private GameObject elevator;
-
-    private float saturation = 0.8f;
-    private List<PC> PCs; 
-
-    
+    protected List<PC> PCs = new List<PC>(); //populated from each child PC.
+    private const int goalReboots = 15;
+    public int reboots = 0;
 
 	// Use this for initialization
 	void Start ()
 	{
+        //rand time
+        //Random.seed = 42;
 
-        //size of room
-        room = this.GetComponent<Renderer>().bounds;
-
-        //size of elevator
-	    elevator = GameObject.Find("Elevator");
-	    float eleRight = elevator.transform.position.x + elevator.GetComponent<Renderer>().bounds.extents.x;
-
-        //size of pc
-	    go = Resources.Load<GameObject>("Prefabs/PC/PC");
-        pc = go.transform.Find("Rect").GetComponent<Renderer>().bounds;
-
-        //rand spawn
-        float consumed = 0f;
-        Random.seed = 42;
-	    float dumbConsumed = 0;
-
-        while (consumed < saturation)
-        {
-            Vector3 position = new Vector3(Random.Range(-room.extents.x - eleRight, room.extents.x), 0, 0);
-            Quaternion plain = Quaternion.identity;
-            Instantiate(go, position, plain);
-
-            dumbConsumed += pc.size.x;
-            consumed = dumbConsumed/room.size.x;
-        }
-        Debug.Log(room.size + ", " + pc.size);
 
 	}
 
     // Update is called once per frame
     void Update () {
-	
-	}
+
+    }
 }
