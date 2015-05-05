4c942692987e771c0185d75db3741882e9caee47
Ian Nastajus <ian.nastajus@gmail.com>
2015-05-04 23:05:32 -0400

    escape exe, promote sound on boss kill now

---
+++ Assets/Scripts/Boss/BossWall.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BossWall : MonoBehaviour {
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.name == "Boss")
+        {
+            SoundManager.Play(SoundManager.Sounds.Promotion);
+        }
+    }
+}
